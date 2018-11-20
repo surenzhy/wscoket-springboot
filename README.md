4f3cb905afb04614e8286adcc23b15f7bb551aff
SanjivSingh <sanjiv.singh.79@gmail.com>
2018-11-20 23:45:17 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# wscoket-springboot
---
+++ src/main/java/ws/WebSocketController.java
@@ -0,0 +1,25 @@
+package ws;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebSocketController {
+
+    private final SimpMessagingTemplate template;
+
+    @Autowired
+    WebSocketController(SimpMessagingTemplate template){
+        this.template = template;
+    }
+
+    @MessageMapping("/send/message")
+    public void onReceivedMesage(String message){
+        this.template.convertAndSend("/mtpoic",  new SimpleDateFormat("HH:mm:ss").format(new Date())+"- "+message);
+    }
+}
